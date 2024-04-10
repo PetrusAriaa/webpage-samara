801c9490573c05347fc2a3abc9d60e261a5bab7f
PetrusAriaa <petrusariacr25@gmail.com>
2024-04-11 02:49:55 +0700

    chore: setup nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
+const { nextui } = require("@nextui-org/theme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|navbar|ripple|spinner).js"
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        serif: ['var(--font-trajan)'],
+        sans: ['var(--font-jakarta)'],
       },
+      colors: {
+        "blue-primary": "#1A3783",
+        "blue-secondary": "#0071B7",
+        "white-secondary": "#EFEFEF",
+        "dark-primary": "#0A1124",
+      }
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
