af877a079c97ca54b322abf3b68a59080a2ad26a
PetrusAriaa <petrusariacr25@gmail.com>
2024-04-20 02:51:17 +0700

    feat: create placeholder section for agenda and foto

---
+++ src/app/sections/AgendaParoki.jsx
@@ -0,0 +1,15 @@
+const AgendaParokiSection = () => {
+  return (
+    <div className="w-full flex justify-center py-16">
+      <div className="w-[95%] max-w-[1300px]">
+        <div className="flex items-center gap-10 mb-16">
+          <div className="w-full h-[2px] bg-neutral-300" />
+          <h1 className="font-serif text-2xl text-center min-w-max text-neutral-500">Agenda Paroki</h1>
+          <div className="w-full h-[2px] bg-neutral-300" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AgendaParokiSection
