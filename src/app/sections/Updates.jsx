eb0887e2ec4ac95434a737208d486379aeba5ef4
PetrusAriaa <petrusariacr25@gmail.com>
2024-04-30 09:56:52 +0700

    feat: add social links

--- src/app/sections/Updates.jsx
+++ src/app/sections/Updates.jsx
@@ -1,13 +1,30 @@
 import { Button } from "@nextui-org/button"
 import Image from "next/image"
 import Link from "next/link"
+import { FaWhatsapp, FaPhoneAlt, FaInstagram, FaYoutube } from "react-icons/fa"
+import { FiMail } from "react-icons/fi"
 
 const UpdatesSection = () => {
   return (
     <>
-      <div className="flex justify-center bg-blue-primary py-2">
-        <div className="w-[95%] max-w-[1300px] text-white flex">
-          Media Sosial
+      <div className="flex justify-center bg-blue-primary py-1">
+        <div className="w-[95%] max-w-[1300px] text-white flex justify-center items-center gap-4">
+          <div className="flex items-center gap-2">
+            <FaPhoneAlt className="text-sm" />
+            <p className="text-xs">(021) 5411509</p>
+          </div>
+          <Link href="mailto:sekretariat@parokikosambibaru.or.id">
+            <FiMail className="text-lg hover:text-blue-secondary transition-colors" />
+          </Link>
+          <Link href="https://wa.me/6281806999631" target="_blank">
+            <FaWhatsapp className="text-lg hover:text-blue-secondary transition-colors" />
+          </Link>
+          <Link href="http://instagram.com/paroki.kosambibaru" target="_blank">
+            <FaInstagram className="text-lg hover:text-blue-secondary transition-colors" />
+          </Link>
+          <Link href="https://www.youtube.com/@GerejaSantoMatiasRasul" target="_blank">
+            <FaYoutube className="text-xl hover:text-blue-secondary transition-colors" />
+          </Link>
         </div>
       </div>
       <div className="w-full bg-dark-primary flex justify-between">
