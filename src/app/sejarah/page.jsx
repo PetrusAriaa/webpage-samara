cf0ced4aa18833bc2299cbfd97309eb640f63f4f
kderrylo <derrylzodxyt@gmail.com>
2024-11-16 02:00:27 +0700

    feat: mobile responsive

--- src/app/sejarah/page.jsx
+++ src/app/sejarah/page.jsx
@@ -3,20 +3,20 @@ import Image from "next/image"
 const Header = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="w-full h-[60vh] max-h-[720px] relative">
+      <div className="w-full h-[35vh] md:h-[40vh] lg:h-[50vh] max-h-[720px] relative">
         <div className="relative w-full h-full bg-blue-950">
           <Image
             src="/assets/sejarah-paroki_01.webp"
             alt="hero"
-            className="object-cover" 
+            className="object-cover"
             priority
             fill />
         </div>
       </div>
-      <div className="bg-blue-950/65 w-full h-[60vh] max-h-[720px] absolute flex justify-center">
-        <div className="w-[90%] max-w-[1300px] text-white h-full flex flex-col items-center justify-end">
-          <div className="flex flex-col items-center gap-4 mb-16">
-            <h1 className="font-heading text-6xl text-center min-w-max "><b>Sejarah</b> Gereja</h1>
+      <div className="bg-blue-950/50 w-full h-[35vh]  md:h-[40vh] lg:h-[50vh] max-h-[720px] absolute flex justify-center">
+        <div className="w-[90%] max-w-[1300px] text-white h-full flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-4 mt-20  ">
+            <h1 className="font-heading text-4xl sm:text-3xl md:text-4xl lg:text-5xl text-center min-w-max "><b>Sejarah</b> Gereja</h1>
             <div className="w-8 h-1.5 bg-white" />
           </div>
         </div>
@@ -29,17 +29,17 @@ const ContentSection = () => {
   return (
     <div className="flex w-full justify-center bg-white">
       <div className="w-4/5 max-w-[1300px] flex">
-        <div className="relative w-[30%] border-r-3 border-blue-primary flex-shrink-0 flex justify-end items-center">
+        <div className="relative w-[20%] border-r-3 border-blue-primary flex-shrink-0 flex justify-end items-center">
           <div className="flex flex-row items-center gap-4 origin-bottom-right -rotate-90 pb-4">
             <div className="size-4 bg-blue-primary rounded-full ring-2 ring-offset-4 ring-blue-secondary" />
             <h2 className="text-2xl font-heading font-semibold text-blue-primary leading-none">1990-1995</h2>
           </div>
         </div>
-        <div className="py-20 px-6 flex flex-col gap-4 text-justify">
+        <div className="py-20 px-6 lg:text-sm text-xs flex flex-col gap-4 text-justify">
           <p>Setelah melalui penelitian geografis, pihak keuskupan Agung Jakarta, pada saat itu Mgr.Leo Seokoto, SJ (Alm) menugaskan Romo LBS Wiryowardoyo, Pr sebagai Pastor Kepala Paroki Santo Thomas Rasul Bojong Indah, untuk memekarkan paroki ke arah Barat, mengingat potensi perkembangan jumlah umat yang sangat besar seiring perkembangan jumlah penduduk di kawawan Jakarta Barat, dengan munculya beberapa perumahan baru, antara lain Interkota Indah, Cantiga, Kresek, Kosambi Baru dan Semanan Indah.</p>
-          <h3 className="font-heading text-sm w-max bg-blue-secondary/20 text-blue-secondary px-3 py-1.5 rounded-full">9 September 1990</h3>
+          <h3 className="font-heading w-max bg-blue-secondary/20 text-blue-secondary px-3 py-1.5 rounded-full">9 September 1990</h3>
           <p>Pada tanggal 9 September 1990 dibentuklah Panitia Pembangunan Gereja (PPG) dengan ketua umumnya Bapak Jopie Sarwono (Alm). Kepanitiaan ini mulai mencari tanah untuk lokasi gereja, diantaranya tanah seluas 4500 m2 di Perumahan Kosambi Baru, milik PT.Metropolitan Development. Rencana pembelian ini telah dilaporkan ke Keuskupan Agung Jakarta dan telah terjadi beberapa kali pertemuan dengan pemilik dan peninjauan ke lokasi oleh Romo Vikjen Soenarwijaya, Pr (Alm) pada saat itu, ternyata pencarian tahan untuk lokasi gereja masih belum berhasil.</p>
-          <h3 className="font-heading text-sm w-max bg-blue-secondary/20 text-blue-secondary px-3 py-1.5 rounded-full">1995</h3>
+          <h3 className="font-heading w-max bg-blue-secondary/20 text-blue-secondary px-3 py-1.5 rounded-full">1995</h3>
           <p>Mengingat kesehatannya makin menurun, pada tahun 1995 Bapak Jopie Sarwono mengundurkan diri dari kepanitiaan, maka sejak itu kepanitiaan ini menjadi terhenti. Kepanitian ini telah memberikan ide tentang pentingnya akan kebutuhan pemekaran paroki bagi pertumbuhan jumlah umat yang pesat.</p>
         </div>
       </div>
@@ -51,14 +51,14 @@ const ContentSection2 = () => {
   return (
     <div className="flex w-full justify-center bg-white-secondary">
       <div className="w-4/5 max-w-[1300px] flex">
-        <div className="relative w-[30%] border-r-3 border-blue-primary flex-shrink-0 flex justify-end items-center">
+        <div className="relative w-[20%] border-r-3 border-blue-primary flex-shrink-0 flex justify-end items-center">
           <div className="flex flex-row items-center gap-4 origin-bottom-right -rotate-90 pb-4">
             <div className="size-4 bg-blue-primary rounded-full ring-2 ring-offset-4 ring-blue-secondary" />
             <h2 className="text-2xl font-heading font-semibold text-blue-primary leading-none">1996-1999</h2>
           </div>
         </div>
-        <div className="py-20 px-6 flex flex-col gap-4 text-justify">
-          <h3 className="font-heading text-sm w-max bg-blue-secondary/20 text-blue-secondary px-3 py-1.5 rounded-full">24 Desember 1996</h3>
+        <div className="py-20 px-6 lg:text-sm text-xs flex flex-col gap-4 text-justify">
+          <h3 className="font-heading lg:text-sm text-xs w-max bg-blue-secondary/20 text-blue-secondary px-3 py-1.5 rounded-full">24 Desember 1996</h3>
           <p>Pada tanggal 24 Desember 1996, Romo TAM Rochadi, Pr Pastor Kepala Paroki Santo Thomas Rasul pengganti Romo LBS Wiryowardoyo, Pr membentuk kembali PPG dengan ketua umumnya Bp Benny Tani, kepanitiaan ini mencari lokasi tanah di daerah Taman Semanan Indah dan juga perijinan bagi pembangunan gereja akan tetapi belum juga berhasil mendapatkannya.</p>
           <div>
             <div className="relative w-3/5 aspect-[5/4] bg-neutral-300 rounded-lg overflow-hidden">
@@ -82,13 +82,13 @@ const ContentSection3 = () => {
   return (
     <div className="flex w-full justify-center bg-white">
       <div className="w-4/5 max-w-[1300px] flex">
-        <div className="relative w-[30%] border-r-3 border-blue-primary flex-shrink-0 flex justify-end items-center">
+        <div className="relative w-[20%] border-r-3 border-blue-primary flex-shrink-0 flex justify-end items-center">
           <div className="flex flex-row items-center gap-4 origin-bottom-right -rotate-90 pb-4 -translate-y-20">
             <div className="size-4 bg-blue-primary rounded-full ring-2 ring-offset-4 ring-blue-secondary" />
             <h2 className="text-2xl font-heading font-semibold text-blue-primary leading-none">2000-sekarang</h2>
           </div>
         </div>
-        <div className="py-20 px-6 flex flex-col w-full gap-4 text-justify">
+        <div className="py-20 px-6 lg:text-sm text-xs flex flex-col w-full gap-4 text-justify">
           <h3 className="text-xl font-heading text-blue-secondary">Peta Wilayah Paroki Kosambi Baru</h3>
           <div className="w-full h-[26rem] bg-neutral-300 rounded-lg animate-pulse" >
             <iframe src="https://www.google.com/maps/d/embed?mid=1hB2Spsg2Jozjy0NUO19lz7-Kr4ICcAg&ehbc=2E312F&scrollwheel=true" height="100%"
--- src/app/visi-misi/page.jsx
+++ src/app/visi-misi/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 const Header = () => {
   return (
     <div className="w-full flex justify-center">
-      <div className="w-full h-[50vh] max-h-[720px] relative">
+      <div className="w-full h-[35vh] md:h-[40vh] lg:h-[50vh] max-h-[720px] relative">
         <div className="relative w-full h-full bg-blue-950">
           <Image
             src="/assets/VISI-DAN-MISI_02.png"
@@ -13,7 +13,7 @@ const Header = () => {
             fill />
         </div>
       </div>
-      <div className="bg-blue-950/50 w-full h-[50vh] max-h-[720px] absolute flex justify-center">
+      <div className="bg-blue-950/50 w-full h-[35vh]  md:h-[40vh] lg:h-[50vh] max-h-[720px] absolute flex justify-center">
         <div className="w-[90%] max-w-[1300px] text-white h-full flex flex-col items-center justify-center">
           <div className="flex flex-col items-center gap-4 mt-20  ">
             <h1 className="font-heading text-4xl sm:text-3xl md:text-4xl lg:text-5xl text-center min-w-max "><b>Visi</b> dan Misi</h1>
@@ -39,7 +39,7 @@ const Visi = () => {
       {/* <div className="flex gap-16 flex-col justify-center items-end max-w-[650px] px-6"> */}
       <div className="bg-white px-2 lg:px-6 flex gap-7 lg:gap-16 flex-col justify-center max-w-[650px] py-7 lg:py-20">
         <h2 className="text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-bold font-heading text-blue-primary text-right">Visi</h2>
-        <p className="text-xs lg:text-2xl text-right">Meningkatkan kualitas hidup umat beriman dalam keluarga dan masyarakat dengan karya nyata, inovatif, dan dalam terang Roh Kudus sehingga kehadiran Allah dapat lebih dirasakan berdasarkan semangat Gembala Baik dan murah hati.</p>
+        <p className="text-xs md:text-1xl lg:text-2xl  text-right">Meningkatkan kualitas hidup umat beriman dalam keluarga dan masyarakat dengan karya nyata, inovatif, dan dalam terang Roh Kudus sehingga kehadiran Allah dapat lebih dirasakan berdasarkan semangat Gembala Baik dan murah hati.</p>
         {/* text-2xl sm:text-1xl md:text-2xl lg:text-5xl */}
       </div>
     </div>
