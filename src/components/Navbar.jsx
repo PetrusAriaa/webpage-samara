2b7285404b19f6539bf4dc3c78975b75137901a1
kderrylo <derrylzodxyt@gmail.com>
2024-11-26 16:41:33 +0700

    fix dropdown 'profil'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { Dropdown, DropdownItem, DropdownTrigger, DropdownMenu , Button} from "@nextui-org/react"; 
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -8,29 +8,27 @@ import {
   NavbarMenu,
   NavbarMenuItem,
   NavbarMenuToggle,
+
 } from "@nextui-org/navbar";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { BsList, BsXLg } from "react-icons/bs";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const Navbar = () => {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [scrollPos, setScrollPos] = useState(0);
-  // const [scrollPos, setScrollPos] = useState(window.scrollY);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
       setScrollPos(window.scrollY);
-    })
-  }, [])
+    });
+  }, []);
 
-  const path = usePathname()
-  const shouldRenderNav = path === "/"
-  const scrollTreshold = shouldRenderNav ? 200 : 50
-
+  const path = usePathname();
+  const shouldRenderNav = path === "/";
+  const scrollTreshold = shouldRenderNav ? 200 : 50;
 
   return path.split("/").indexOf("zaitun") > -1 ? null : (
     <NextUINavbar
@@ -39,7 +37,9 @@ const Navbar = () => {
       isBordered
       isBlurred={false}
       classNames={{
-        base: (scrollPos > scrollTreshold ? "lg:py-0 " : "lg:py-4 ") + " bg-white fixed py-0 border-b border-slate-200/50 transition-all shadow-lg drop-shadow-sm",
+        base:
+          (scrollPos > scrollTreshold ? "lg:py-0 " : "lg:py-4 ") +
+          " bg-white fixed py-0 border-b border-slate-200/50 transition-all shadow-lg drop-shadow-sm",
         wrapper: "w-[90%] max-w-[1300px]",
       }}
     >
@@ -47,49 +47,68 @@ const Navbar = () => {
         <NavbarMenuToggle
           icon={isMenuOpen ? <BsXLg size={30} /> : <BsList size={30} />}
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="w-fit aspect-square flex lg:hidden hover:bg-blue-secondary/10 text-blue-secondary h-min p-2" />
+          className="w-fit aspect-square flex lg:hidden hover:bg-blue-secondary/10 text-blue-secondary h-min p-2"
+        />
         <NavbarBrand className="hidden sm:block w-fit ">
-            <Link href="/">
-              <div className={
-                (scrollPos > scrollTreshold ? "lg:h-14 " : "lg:h-20 ") + 
+          <Link href="/">
+            <div
+              className={
+                (scrollPos > scrollTreshold ? "lg:h-14 " : "lg:h-20 ") +
                 " relative h-14 aspect-[54/10] transition-all"
-                }>
-                <Image src="/static/logo_50.svg" alt="" fill className="object-cover"/>
-              </div>
-            </Link>
+              }
+            >
+              <Image src="/static/logo_50.svg" alt="" fill className="object-cover" />
+            </div>
+          </Link>
         </NavbarBrand>
       </NavbarContent>
       <NavbarContent className="hidden lg:flex gap-8" justify="end">
         <NavbarItem>
           <Link
             href="/"
-            className={
-              // (scrollPos > scrollTreshold ? "lg:text-neutral-dark " : "lg:text-white ") +
-              "transition-colors text-neutral-dark hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
-            }
+            className="transition-colors text-neutral-dark hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
           >
             Beranda
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link
-            href="#"
-            className={
-              // (scrollPos > scrollTreshold ? "lg:text-neutral-dark " : "lg:text-white ") +
-              "transition-colors text-neutral-dark hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
-            }
-          >
-            Profil
-          </Link>
+          <Dropdown>
+            <DropdownTrigger>
+              <Button
+                className="bg-transparent h-auto transition-colors text-neutral-dark hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
+              >
+                Profil
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profil Menu" >
+              <DropdownItem>
+                <Link href="/visi-misi" className="w-full py-2 px-4">
+                  Visi dan Misi
+                </Link>
+              </DropdownItem>
+              <DropdownItem>
+                <Link href="/sejarah" className="w-full py-2 px-4">
+                  Sejarah Paroki
+                </Link>
+              </DropdownItem>
+              <DropdownItem>
+                <Link href="/profil-pastor" className="w-full py-2 px-4">
+                  Profil Pastor
+                </Link>
+              </DropdownItem>
+              <DropdownItem>
+                <Link href="#" className="w-full py-2 px-4 text-neutral-400 cursor-not-allowed">
+                  Susunan Pengurus
+                </Link>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarItem>
         <NavbarItem>
           <Link
             href="/zaitun"
             target="_blank"
-            className={
-              // (scrollPos > scrollTreshold ? "lg:text-neutral-dark " : "lg:text-white ") +
-              "transition-colors text-neutral-dark hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
-            }
+            className="transition-colors text-neutral-dark hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
           >
             Zaitun
           </Link>
@@ -97,10 +116,7 @@ const Navbar = () => {
         <NavbarItem>
           <Link
             href="#"
-            className={
-              // (scrollPos > scrollTreshold ? "lg:text-neutral-dark " : "lg:text-white ") +
-              "transition-colors text-neutral-400 cursor-not-allowed hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
-            }
+            className="transition-colors text-neutral-400 cursor-not-allowed hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
           >
             Jadwal
           </Link>
@@ -108,10 +124,7 @@ const Navbar = () => {
         <NavbarItem>
           <Link
             href="/berita"
-            className={
-              // (scrollPos > scrollTreshold ? "lg:text-neutral-dark " : "lg:text-white ") +
-              "transition-colors text-neutral-dark hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
-            }
+            className="transition-colors text-neutral-dark hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
           >
             Berita
           </Link>
@@ -119,10 +132,7 @@ const Navbar = () => {
         <NavbarItem>
           <Link
             href="#"
-            className={
-              // (scrollPos > scrollTreshold ? "lg:text-neutral-dark " : "lg:text-white ") +
-              "transition-colors text-neutral-400 cursor-not-allowed hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
-            }
+            className="transition-colors text-neutral-400 cursor-not-allowed hover:bg-blue-secondary/25 hover:!text-blue-secondary rounded-md px-3 py-1"
           >
             Galeri
           </Link>
@@ -130,12 +140,20 @@ const Navbar = () => {
       </NavbarContent>
       <NavbarMenu className="text-neutral-dark p-0 gap-0 h-fit">
         <NavbarMenuItem className="flex">
-          <Link onClick={() => setIsMenuOpen(false)} href="/" className="w-full hover:bg-teal-secondary/75 hover:text-white transition-colors py-2 px-4 border-b border-slate-200">
+          <Link
+            onClick={() => setIsMenuOpen(false)}
+            href="/"
+            className="w-full hover:bg-teal-secondary/75 hover:text-white transition-colors py-2 px-4 border-b border-slate-200"
+          >
             Home
           </Link>
         </NavbarMenuItem>
         <NavbarMenuItem className="flex">
-          <Link onClick={() => setIsMenuOpen(false)} href="/about" className="w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-4 border-b border-slate-200">
+          <Link
+            onClick={() => setIsMenuOpen(false)}
+            href="/about"
+            className="w-full hover:bg-teal-secondary/75 hover:text-white transition-colors text-xl py-2 px-4 border-b border-slate-200"
+          >
             About Us
           </Link>
         </NavbarMenuItem>
