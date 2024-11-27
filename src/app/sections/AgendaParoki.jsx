73665a9431fa5f21f7f6d7037e6e6c80d905276b
kderrylo <derrylzodxyt@gmail.com>
2024-11-27 11:09:38 +0700

    dashboard berita

--- src/app/sections/AgendaParoki.jsx
+++ src/app/sections/AgendaParoki.jsx
@@ -1,10 +1,37 @@
+"use client";
+
+import { notFound } from "next/navigation";
+import { BeritaItem } from "@/app/ui/berita/berita-item";
+import { useEffect, useState } from "react";
+
 const AgendaParokiSection = () => {
+  const [berita, setberita] = useState(null);
+
+  useEffect(() => {
+    const fetchberita = async () => {
+      const response = await fetch(`/api/berita`);
+      const berita = await response.json();
+
+      if (!berita) return notFound();
+
+      setberita(berita || null);
+    };
+    fetchberita();
+  }, []);
+
+  if (!berita) return <div>Tidak ada berita</div>;
   return (
     <div className="w-full flex justify-center py-16 bg-white-secondary">
       <div className="w-[95%] max-w-[1300px]">
         <div className="flex flex-col items-center mb-16">
           <h1 className="font-heading text-[2rem] text-center min-w-max text-blue-primary"><b>Artikel</b> Terbaru</h1>
           <div className="w-8 h-1.5 bg-blue-primary" />
+          <div className="grid grid-cols-2 py-2 gap-2 mx-2 sm:grid-cols-3 sm:py-3 sm:gap-3 sm:mx-3 md:grid-cols-3 md:py-6 md:gap-4 md:mx-3 lg:grid-cols-4 lg:py-8 lg:gap-6 lg:mx-4">
+          {berita.map((item) => (
+            <BeritaItem key={item.id} item={item} />
+          ))}
+        </div>
+          
         </div>
       </div>
     </div>
