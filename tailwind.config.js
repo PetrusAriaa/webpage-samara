1cf0d9e10627dd5ab68d73bb6869183267534482
kderrylo <derrylzodxyt@gmail.com>
2024-11-28 20:19:41 +0700

    add nextui dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(button|dropdown|input|modal|navbar|ripple|spinner|menu|divider|popover).js"
+    "./node_modules/@nextui-org/theme/dist/components/(button|card|dropdown|input|modal|navbar|ripple|spinner|menu|divider|popover).js"
   ],
   theme: {
     extend: {
